3b398375b0bf817107fa1980af4fd8d351d45507
Sohini Pillay <sohinipillay@Sohinis-MacBook-Pro.local>
2024-04-09 18:06:54 -0700

    updates

---
+++ app/data.js
@@ -0,0 +1,31 @@
+export const quiz = {
+    totalQuestions: 1,
+    questions: [
+      {
+        id: 1,
+        question: 'What is your favorite type of vacation?',
+        answers: {
+          A: 'Exploring big cities',
+          B: 'Relaxing on the beach',
+          C: 'Hiking in the mountains',
+          D: 'Visiting historical sites'
+        },
+      },
+      {
+        id: 2,
+        question: 'What kind of movies do you prefer?',
+        answers: {
+          A: 'Action and adventure',
+          B: 'Romantic comedies',
+          C: 'Documentaries',
+          D: 'Science fiction'
+        },
+      },
+    ],
+    results: {
+      A: 'Outcome for mostly A answers',
+      B: 'Outcome for mostly B answers',
+      C: 'Outcome for mostly C answers',
+      D: 'Outcome for mostly D answers',
+    }
+  };
